da7fc1268de9addab3814edfb4da64110b7f552e
NiKuma0 <ybr811@gmail.com>
2025-10-27 23:57:43 +0300

    Add docs strings

--- src/routers/organizations.py
+++ src/routers/organizations.py
@@ -1,3 +1,5 @@
+import typing
+
 import fastapi
 
 from src import schemas
@@ -28,13 +30,14 @@ async def get_by_building_address(
 
 @router.get('/radius')
 async def get_by_building_location_radius(
-    lon: float,
-    lat: float,
-    radius_m: int,
+    lon: typing.Annotated[float, fastapi.Query(description='Longitude')],
+    lat: typing.Annotated[float, fastapi.Query(description='Latitude')],
+    radius_m: typing.Annotated[int, fastapi.Query(description='Radius of search in meters')],
     service: OrganizationServiceDep,
     limit: int = 10,
     offset: int = 0,
 ) -> schemas.ListOrganizations:
+    """Get organizations by its location in area."""
     return await service.get_by_building_location_radius(
         latitude=lat, longitude=lon, radius_m=radius_m, limit=limit, offset=offset
     )
@@ -42,14 +45,15 @@ async def get_by_building_location_radius(
 
 @router.get('/box')
 async def get_by_building_location_box(
-    ll_lon: float,
-    ll_lat: float,
-    ur_lon: float,
-    ur_lat: float,
+    ll_lon: typing.Annotated[float, fastapi.Query(description='Low left longitude')],
+    ll_lat: typing.Annotated[float, fastapi.Query(description='Low left latitude')],
+    ur_lon: typing.Annotated[float, fastapi.Query(description='Upper right longitude')],
+    ur_lat: typing.Annotated[float, fastapi.Query(description='Upper right latitude')],
     service: OrganizationServiceDep,
     limit: int = 10,
     offset: int = 0,
 ) -> schemas.ListOrganizations:
+    """Get organizations by its location in box area."""
     return await service.get_by_building_location_box(
         ll_longitude=ll_lon,
         ll_latitude=ll_lat,
@@ -62,7 +66,7 @@ async def get_by_building_location_box(
 
 @router.get('/specs', operation_id='get_by_specializations')
 async def get_by_specializations(
-    specs: list[int],
+    specs: typing.Annotated[list[int], fastapi.Query(description='Ids of specializations')],
     service: OrganizationServiceDep,
     limit: int = 10,
     offset: int = 0,
